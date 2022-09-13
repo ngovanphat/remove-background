19b380979b67d2586a2c7d618bb26a5bc6ef10e3
phat-nv <ngo.van.phat@codelynx.io>
2022-09-13 21:45:07 +0700

    add firebase hosting

---
+++ src/config/firebase.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3IByd3IRiDYPiOHkhlYy13XhWxixG4gE",
+    authDomain: "remove-background-132b6.firebaseapp.com",
+    projectId: "remove-background-132b6",
+    storageBucket: "remove-background-132b6.appspot.com",
+    messagingSenderId: "775819622073",
+    appId: "1:775819622073:web:b92d78ba77232d704d0d68",
+    measurementId: "G-BPMKHY6P9K"
+  };
+
+export default firebaseConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import firebaseConfig from '@/config/firebase'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

be892eda0718690899cbe8bfcff4623a7c634f9e
phat-nv <ngo.van.phat@codelynx.io>
2022-12-12 10:50:22 +0700

    add upload background feature

--- src/components/Uploader/Uploader.tsx
+++ src/components/Uploader/Uploader.tsx
@@ -8,6 +8,7 @@ import { useDropzone } from "react-dropzone";
 import { Typography } from 'antd';
 
 import './Uploader.css';
+import colorPickerIcon from '@/assets/icons/color-picker-icon.svg';
 
 
 const { Title } = Typography;
@@ -15,17 +16,15 @@ const { Title } = Typography;
 const Uploader = ({...params}) => {
     const apiURL = import.meta.env.VITE_APP_API_URL;
     const passedImage = params.image;
+    const colors = ['transparent','#ffffff','#000000','#d291ff','#de3163','#eaddca','other'] as string[]
+
 
     const [loading, setLoading] = useState(false);
     const [image, setImage] = useState('');
     const [resultImage, setResultImage] = useState('');
     const [tabIndex, setTabIndex] = useState(1);
     const [color, setColor] = useState('');
-
-    const imageSize ={
-        width: 500,
-        height: 500,
-    }
+    const [backgroundUrl, setBackgroundUrl] = useState('src/assets/checkerboard.png') 
 
     const onDrop = useCallback((acceptedFiles: any) => {
         handleDrop(acceptedFiles[0]);
@@ -49,13 +48,13 @@ const Uploader = ({...params}) => {
     const beforeUpload = (file: any) => {
         const isImage = file.type.indexOf('image/') === 0;
         if (!isImage) {
-            message.error('You can only upload image file!');
+            message.error('Tụi mình chỉ hỗ trợ xử lý ảnh thôi bạn nhé!');
         }
         
         // You can remove this validation if you want
-        const isLt5M = file.size / 1024 / 1024 < 5;
+        const isLt5M = file.size / 1024 / 1024 < 25;
         if (!isLt5M) {
-            message.error('Ảnh nên nhỏ hơn 5MB!');
+            message.error('Ảnh nên nhỏ hơn 25MB!');
         }
         return isImage && isLt5M;
     };
@@ -99,33 +98,56 @@ const Uploader = ({...params}) => {
         switch (colorNumber) {
             case 1: {
                 setColor('transparent');
+                setBackgroundUrl('src/assets/checkerboard.png')
                 break;
             }
             case 2: {
-                setColor('white');
+                setColor(colors[1]);
                 break;
             }
             case 3: {
-                setColor('black');
+                setColor(colors[2]);
                 break;
             }
             case 4: {
-                setColor('purple');
+                setColor(colors[3]);
                 break;
             }
             case 5: {
-                setColor('pink');
+                setColor(colors[4]);
                 break;
             }
             case 6: {
-                setColor('yellow');
+                setColor(colors[5]);
                 break;
             }
             case 7: {
-                setColor('picker');
+                const colorInput = document.getElementById('color-picker-input') as HTMLInputElement;
+                setColor(colorInput.value || '#000000')
+                colorInput?.click();
+                break;
+            }
+            case 8: {
+                setColor('other');
                 break;
             }
         }
+    }
+
+    const onChangePicker = (event: any) => {
+        setColor(event.target.value)
+    }
+
+    const uploadBackground = () => {
+        pickColor(8);
+        const uploadBackgroundInput = document.getElementById("upload-background") as HTMLInputElement;
+        uploadBackgroundInput.click();
+    }
+
+    const handleUploadNewBackground = (e: any) => {
+        const file = e.target.files[0]
+        if (beforeUpload(file))
+            setBackgroundUrl(URL.createObjectURL(e.target.files[0]))
     }
     return (
         <div className="upload-section">
@@ -136,12 +158,31 @@ const Uploader = ({...params}) => {
                 {resultImage && <div className="action-container">
                     <div className="action-row">
                         <div className={("color-circle transparent ") + (color === 'transparent' ? 'color-active' : '')} onClick={() => pickColor(1)}></div>
-                        <div className={("color-circle white ") + (color === 'white' ? 'color-active' : '')} onClick={() => pickColor(2)}></div>
-                        <div className={("color-circle black ") + (color === 'black' ? 'color-active' : '')} onClick={() => pickColor(3)}></div>
-                        <div className={("color-circle purple ") + (color === 'purple' ? 'color-active' : '')} onClick={() => pickColor(4)}></div>
-                        <div className={("color-circle pink ") + (color === 'pink' ? 'color-active' : '')} onClick={() => pickColor(5)}></div>
-                        <div className={("color-circle yellow ") + (color === 'yellow' ? 'color-active' : '')} onClick={() => pickColor(6)}></div>
-                        <div className={("color-circle picker ") + (color === 'picker' ? 'color-active' : '')} onClick={() => pickColor(7)}></div>
+                        <div className={("color-circle white ") + (color === colors[1] ? 'color-active' : '')} onClick={() => pickColor(2)}></div>
+                        <div className={("color-circle black ") + (color === colors[2] ? 'color-active' : '')} onClick={() => pickColor(3)}></div>
+                        <div className={("color-circle purple ") + (color === colors[3] ? 'color-active' : '')} onClick={() => pickColor(4)}></div>
+                        <div className={("color-circle pink ") + (color === colors[4] ? 'color-active' : '')} onClick={() => pickColor(5)}></div>
+                        <div className={("color-circle yellow ") + (color === colors[5] ? 'color-active' : '')} onClick={() => pickColor(6)}></div>
+                        <div className={("color-circle picker ") + (!colors.includes(color) ? 'color-active' : '')} onClick={() => pickColor(7)}>
+                            <img src={colorPickerIcon} alt="icon" style={{
+                                width: '15px',
+                                height: '15px'
+                            }}/>
+                            <input type="color" id="color-picker-input" name="head"
+                            style={{zIndex: -1, 
+                                top: '-20px',
+                                position: 'absolute', width: '0', height: '5px'}}
+                            value={color}
+                            onChange={(value) => onChangePicker(value)}
+                            ></input>
+                        </div>
+
+                        <div className={"color-circle picker add-background " + (color == 'other' ? 'color-active' : '')} onClick={() => uploadBackground()}>
+                            <input type="file" id="upload-background" 
+                            onChange={handleUploadNewBackground}
+                            style={{display: 'none'}} />
+                            <PlusOutlined/>
+                        </div>
                     </div>
                     <div className="action-row">
                         <div className="tab-controller">
@@ -177,7 +218,7 @@ const Uploader = ({...params}) => {
                 />}
                 {(resultImage && tabIndex === 2) && (<div 
                 id="result-image"
-                className={(color)}>
+                style={{background: !(['other', 'transparent'].includes(color)) ? color : `url(${backgroundUrl})`}}>
                     <AntdImage
                     style={{maxWidth: '600px', maxHeight: '500px'}}
                     src={resultImage}
